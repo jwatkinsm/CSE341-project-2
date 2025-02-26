1d8b194049b7784227b87eccb997468de293b9b6
jwatkinsm <jwatkinsm@gmail.com>
2025-02-26 12:56:54 -0500

    auth0 start

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { auth, requiresAuth } = require("express-openid-connect");
+require("dotenv").config();
 // to parse json data
 const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
@@ -6,6 +8,27 @@ const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 // Use body-parser middleware to parse JSON data
 // Use body-parser middleware to parse JSON data
